e8d974495ddd8e9d5725f2a4c9b72c4a46300878
Daniil Uporov <d.uporov86@gmail.com>
2023-09-13 10:31:05 +0700

    refactoring

--- imperativnoe/1 set of tasks/6.min_max.c
+++ imperativnoe/1 set of tasks/6.min_max.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int min, max, min_index, max_index, amount_numbers, x;
     scanf("%d", &amount_numbers);
     scanf("%d", &x);
-    min = x;
-    max = x;
-    min_index = 1;
-    max_index = 1;
-    for (int i = 1; i < amount_numbers; i++) {
+    min = max = x;
+    min_index = max_index = 1;
+    for (int i = 1; i < amount_numbers; i++)
+    {
         scanf("%d", &x);
-        if (x > max) {
+        if (x > max)
+        {
             max = x;
             max_index = i + 1;
         }
-        if (x < min) {
+        if (x < min)
+        {
             min = x;
             min_index = i + 1;
         }
