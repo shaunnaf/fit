da137cd735ea06959df0c96f61162d7063221595
Daniil <daniil@Daniils-MacBook-Air.local>
2023-09-19 04:05:08 +0700

    add_3_tasks

--- imperativnoe/2 set of tasks/7.Arithmetic progressions.c
+++ imperativnoe/2 set of tasks/7.Arithmetic progressions.c
@@ -2,6 +2,48 @@
 
 int main()
 {
-
+    int left, right, count, curr_count, step, curr_number, answer;
+    answer = 0;
+    step = curr_count = 1;
+    scanf("%d %d %d", &left, &right, &count);
+    for (int i = left; i <= right; i++)
+    {
+        curr_number = i;
+        while (step * (count - 1) <= right - left)
+        {
+            if (curr_count == count)
+            {
+                if (curr_number <= right && (curr_number + step > right))
+                {
+                    answer += 1;
+                    curr_number = i;
+                    curr_count = 1;
+                    step += 1;
+                }
+                else if (curr_number + step <= right)
+                {
+                    step += 1;
+                    curr_count = 1;
+                    curr_number = i;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            else if (curr_number > right)
+            {
+                break;
+            }
+            else
+            {
+                curr_number += step;
+                curr_count++;
+            }
+        }
+        curr_count = 1;
+        step = 1;
+    }
+    printf("%d", answer);
     return 0;
 }
---
+++ imperativnoe/3 set of tasks/1.cmp_elements.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void reading_count(int *count, int *error);
+void reading_numbers(int *error, int count, int *array);
+void counting(int *array, int count);
+
+int main()
+{
+    int count, error;
+    error = 0;
+    reading_count(&count, &error);
+    if (error == 0)
+    {
+        int *array = (int *)malloc(count * sizeof(int));
+        reading_numbers(&error, count, array);
+        if (error == 0)
+        {
+            counting(array, count);
+        }
+        else
+        {
+            printf("ERROR");
+        }
+    }
+    else
+    {
+        printf("ERROR");
+    }
+    return 0;
+}
+
+void reading_count(int *count, int *error)
+{
+    if (scanf("%d", count) != 1 || *count < 1 || *count > 100)
+    {
+        *error = 1;
+    }
+}
+
+void reading_numbers(int *error, int count, int *array)
+{
+    for (int i = 0; i < count; i++)
+    {
+        if (scanf("%d", &array[i]) != 1)
+        {
+            *error = 1;
+            break;
+        }
+    }
+}
+
+void counting(int *array, int count)
+{
+    int answer = 0;
+    for (int i = 0; i < count - 1; i++)
+    {
+        for (int j = i + 1; j < count; j++)
+        {
+            if (array[i] > array[j])
+            {
+                answer += 1;
+            }
+        }
+        printf("%d ", answer);
+        answer = 0;
+    }
+    printf("0");
+}
