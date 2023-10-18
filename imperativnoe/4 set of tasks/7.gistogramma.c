fb97e1c664acd103a9fd4b8eeb1f57e25a7dd314
Daniil <d.uporov86@gmail.com>
2023-10-18 14:01:51 +0700

    add_4_7

---
+++ imperativnoe/4 set of tasks/7.gistogramma.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void calculated_count_symbols(int *array_count_symbols);
+void print_answer(int *array_count_symbols);
+
+int main()
+{
+    int *array_count_symbols = calloc(150, sizeof(int));
+    calculated_count_symbols(array_count_symbols);
+    print_answer(array_count_symbols);
+    free(array_count_symbols);
+    return 0;
+}
+
+void calculated_count_symbols(int *array_count_symbols)
+{
+    char c = getchar();
+    while (c != EOF)
+    {
+        if (c != '\n')
+        {
+            int number = c;
+            array_count_symbols[number] += 1;
+        }
+        c = getchar();
+    }
+}
+
+void print_answer(int *array_count_symbols)
+{
+    for (int i = 32; i < 127; i++)
+    {
+        if (array_count_symbols[i] != 0)
+        {
+            printf("%c ", i);
+            while (array_count_symbols[i] != 0)
+            {
+                printf("#");
+                array_count_symbols[i] -= 1;
+            }
+            printf("\n");
+        }
+    }
+}
